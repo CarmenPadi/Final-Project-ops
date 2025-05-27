3b57443a799538343f168c6f3a5c065aeae61586
CarmenPadi <mc.padillat@gmail.com>
2025-05-27 16:43:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Dockerized Node.js App!\n');
+  // Define the path to index.html
+  const filePath = path.join(__dirname, 'index.html');
+
+  // Read and serve the file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
